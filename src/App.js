1a41a220a725593da4cbd8db17967336ee08ddb2
seog-jun <shong69@myseneca.ca>
2022-04-28 23:28:50 -0400

    Installed Bootstrap

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import "./App.css";
+import { Button, Navbar, Container, Nav } from "react-bootstrap";
+function App() {
+  return (
+    <div className="App">
+      <Navbar bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Home</Nav.Link>
+            <Nav.Link href="#features">Features</Nav.Link>
+            <Nav.Link href="#pricing">Pricing</Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+    </div>
+  );
+}
+
+export default App;
