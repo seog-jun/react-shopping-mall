acb3db39d9caf996435949807a58f00021887432
seog-jun <shong69@myseneca.ca>
2022-04-30 21:17:14 -0400

    Added image and grid

--- src/App.js
+++ src/App.js
@@ -1,18 +1,52 @@
 import "./App.css";
-import { Button, Navbar, Container, Nav } from "react-bootstrap";
+import { Navbar, Container, Nav } from "react-bootstrap";
+import bg from "./img/bg.png";
 function App() {
   return (
     <div className="App">
-      <Navbar bg="dark" variant="dark">
+      <Navbar bg="light" variant="light">
         <Container>
-          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+          <Navbar.Brand href="#home">ShoesMall</Navbar.Brand>
           <Nav className="me-auto">
             <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#features">Features</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
+            <Nav.Link href="#features">Cart</Nav.Link>
           </Nav>
         </Container>
       </Navbar>
+
+      <div
+        className="main-bg"
+        // style={{ backgroundImage: "url(" + bg + ")" }}
+      ></div>
+
+      <div className="Container">
+        <div className="row">
+          <div className="col-md-4">
+            <img
+              src="https://codingapple1.github.io/shop/shoes1.jpg"
+              width="80%"
+            />
+            <h4>Name</h4>
+            <p>Description</p>
+          </div>
+          <div className="col-md-4">
+            <img
+              src="https://codingapple1.github.io/shop/shoes2.jpg"
+              width="80%"
+            />
+            <h4>Name</h4>
+            <p>Description</p>
+          </div>
+          <div className="col-md-4">
+            <img
+              src="https://codingapple1.github.io/shop/shoes3.jpg"
+              width="80%"
+            />
+            <h4>Name</h4>
+            <p>Description</p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
