752cefa7cf8a18dbc49219e9dfde4166b46338e7
seog-jun <shong69@myseneca.ca>
2022-05-01 16:35:25 -0400

    Updated link in Card component

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,7 +1,13 @@
+import { useNavigate } from "react-router-dom";
+
 export default function Card(props) {
+  let navigate = useNavigate();
   return (
     <div className="col-md-4">
       <img
+        onClick={() => {
+          navigate("/detail/" + props.shoes.id + "");
+        }}
         src={
           "https://codingapple1.github.io/shop/shoes" +
           (props.shoes.id + 1) +
