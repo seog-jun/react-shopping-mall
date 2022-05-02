f0d5dfdd03ef06ef8934c3db55fc9e197c378383
seog-jun <shong69@myseneca.ca>
2022-05-01 22:39:00 -0400

    Added useEffect

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,15 +1,32 @@
+import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
-import styled from "styled-components";
 
 export default function Detail(props) {
-  let { id } = useParams();
+  useEffect(() => {
+    let a = setTimeout(() => {
+      setVisible(0);
+    }, 5000);
+    console.log(2);
 
+    return () => {
+      console.log(1);
+      clearTimeout(a);
+    };
+  }, []);
+
+  let [visible, setVisible] = useState(1);
+  let { id } = useParams();
   let shoes = props.shoes.find((o) => {
     return o.id == id;
   });
 
   return (
     <div className="container">
+      {visible ? (
+        <div className="alert alert-warning">
+          Get a discount within 5 seconds!
+        </div>
+      ) : null}
       <div className="row">
         <div className="col-md-6">
           <img
@@ -22,6 +39,7 @@ export default function Detail(props) {
           />
         </div>
         <div className="col-md-6">
+          <input></input>
           <h4 className="pt-5">{shoes.title}</h4>
           <p>{shoes.content}</p>
           <p>{shoes.price}</p>
