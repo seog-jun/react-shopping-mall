ef2c1a080fc6d3c5f7f6dfaf62c049e8d74197be
seog-jun <shong69@myseneca.ca>
2022-05-09 12:48:55 -0400

    Modified using redux toolkit version

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,45 +1,45 @@
-import { combineReducers, createStore } from "redux";
-import cartReducer from "../redux/reducer/cartReducer";
-import alertReducer from "../redux/reducer/alertReducer";
-import countReducer from "../redux/reducer/countReducer";
+// import { combineReducers, createStore } from "redux";
+// import cartReducer from "../redux/reducer/cartReducer";
+// import alertReducer from "../redux/reducer/alertReducer";
+// import countReducer from "../redux/reducer/countReducer";
 
-let store = createStore(
-  combineReducers({ cartReducer, alertReducer, countReducer })
-);
+// let store = createStore(
+//   combineReducers({ cartReducer, alertReducer, countReducer })
+// );
 
-export default store;
+// export default store;
 
-// import { configureStore, createSlice } from "@reduxjs/toolkit";
+import { configureStore, createSlice } from "@reduxjs/toolkit";
 
-// let user = createSlice({
-//   name: "user",
-//   initialState: "kim",
-//   reducers: {
-//     changeName(state) {
-//       return "john kim";
-//     },
-//   },
-// });
+let user = createSlice({
+  name: "user",
+  initialState: "kim",
+  reducers: {
+    changeName(state) {
+      return "john kim";
+    },
+  },
+});
 
-// export let { changeName } = user.actions;
+export let { changeName } = user.actions;
 
-// let stock = createSlice({
-//   name: "stock",
-//   initialState: [10, 11, 12],
-// });
+let stock = createSlice({
+  name: "stock",
+  initialState: [10, 11, 12],
+});
 
-// let cart = createSlice({
-//   name: "cart",
-//   initialState: [
-//     { id: 0, name: "White and Black", count: 2 },
-//     { id: 2, name: "Grey Yordan", count: 1 },
-//   ],
-// });
+let cart = createSlice({
+  name: "cart",
+  initialState: [
+    { id: 0, name: "White and Black", count: 2 },
+    { id: 2, name: "Grey Yordan", count: 1 },
+  ],
+});
 
-// export default configureStore({
-//   reducer: {
-//     user: user.reducer,
-//     stock: stock.reducer,
-//     cart: cart.reducer,
-//   },
-// });
+export default configureStore({
+  reducer: {
+    user: user.reducer,
+    stock: stock.reducer,
+    cart: cart.reducer,
+  },
+});
