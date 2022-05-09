eeeff43a54ea93fed0627e90d6f9438e5f00fbc2
seog-jun <shong69@myseneca.ca>
2022-05-09 12:56:58 -0400

    Updated using toolkit

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,14 +1,3 @@
-// import { combineReducers, createStore } from "redux";
-// import cartReducer from "../redux/reducer/cartReducer";
-// import alertReducer from "../redux/reducer/alertReducer";
-// import countReducer from "../redux/reducer/countReducer";
-
-// let store = createStore(
-//   combineReducers({ cartReducer, alertReducer, countReducer })
-// );
-
-// export default store;
-
 import { configureStore, createSlice } from "@reduxjs/toolkit";
 
 let user = createSlice({
